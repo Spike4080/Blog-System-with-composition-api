b65dcfe3d61e2c9a44cedbe205ba070f4b7f4dbd
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-02-05 17:15:35 +0630

    project init

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{post.title}}</h1>
+  <h3>{{cutPostBody}}</h3>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+    props:['post'],
+
+    setup(props){
+        let cutPostBody = computed(()=>{
+            return props.post.body.substring(0,100) + ".....;"
+        })
+        return {cutPostBody};
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+      <PostsList :posts="posts"></PostsList>
+  </div>
+</template>
+
+<script>
+import PostsList from '../components/PostsList'
+import { computed, ref } from 'vue';
+export default {
+  components: { PostsList },
+  setup() {
+    let posts = ref([]);
+
+    return {posts};
+  }
+}
+</script>
