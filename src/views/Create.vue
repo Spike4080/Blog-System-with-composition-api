fede1a23bcebf039237ebfe87a00e72f36ad3b87
Pyae Sone Paing <www.pyaesonekm2003@gmail.com>
2024-02-06 23:22:50 +0630

    create post backend

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,5 +1,5 @@
 <template>
-    <form>
+    <form @submit.prevent="addPost">
         <label>Title</label>
         <input type="text" required v-model="title">
 
@@ -30,9 +30,24 @@ export default {
                 tags.value.push(tag.value);
             }
             tag.value = "";
+        };
+        let addPost = async()=>{
+            await fetch("http://localhost:3000/posts",{
+                method:"POST",
+                headers:{
+                    "Content-type":"application/json"
+                },
+                body:JSON.stringify(
+                    {
+                        title:title.value,
+                        body:body.value,
+                        tags:tags.value
+                    }
+                )
+            });
         }
 
-        return {title,body,tag,tags,keydownHandler};
+        return {title,body,tag,tags,keydownHandler,addPost};
     }
 }
 </script>
